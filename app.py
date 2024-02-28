705eede5c848276a95e2660c93cfbee26ca677fa
OKyereI <isaacokyere4@gmail.com>
2024-02-28 08:12:29 +0000

    added database

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
-# from flask_sqlalchemy import SQLAlchemy
-import sqlite3 as sql
-from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+
+# c = conn.cursor ()
 
 
-conn = sql.connect("database.db")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -27,14 +26,7 @@ companys=[
         'id':3,'project':'Database Management','status':'available',"location":'Ghana'
     }
 ]
-# app.secret_key = 'your secret key'
- 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = '12345'
-# app.config['MYSQL_DB'] = 'mydb'
- 
-# mysql = MySQL(app)
+
 
 # Creating Models
 class company(db.Model):
@@ -48,6 +40,16 @@ class company(db.Model):
     def __repr__(self) -> str:
         return f"{self.id} -{self.project} - {self.status} - {self.location}"
 
+class Registrants(db.Model):
+    __tablename__ = "account"
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(500), nullable=False, unique=True)
+    email = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"{self.id} -{self.username} - {self.email} - {self.password}"
 
 
 def create_db():
@@ -76,48 +78,33 @@ def add_record():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-    # msg = ''
-    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-    #     username = request.form['username']
-    #     password = request.form['password']
-    #     email = request.form['email']
-    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    #     cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-    #     account = cursor.fetchone()
-    #     if account:
-    #         msg = 'Account already exists !'
-    #     elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-    #         msg = 'Invalid email address !'
-    #     elif not re.match(r'[A-Za-z0-9]+', username):
-    #         msg = 'Username must contain only characters and numbers !'
-    #     elif not username or not password or not email:
-    #         msg = 'Please fill out the form !'
-    #     else:
-    #         cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-    #         mysql.connection.commit()
-    #         msg = 'You have successfully registered !'
-    # elif request.method == 'POST':
-    #     msg = 'Please fill out the form !'
-    return render_template('register.html', msg = msg)
+   
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        db.execute('INSERT INTO account (username, password, email) VALUES (?,?,?)',username, password, email)
+
+        
+    return render_template('register.html')
 
 @app.route("/login", methods=['GET','POST'])
 def login():
-    # msg = ''
-    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-    #     username = request.form['username']
-    #     password = request.form['password']
-    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    #     cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
-    #     account = cursor.fetchone()
-    #     if account:
-    #         session['loggedin'] = True
-    #         session['id'] = account['id']
-    #         session['username'] = account['username']
-    #         msg = 'Logged in successfully !'
-    #         return render_template('index.html', msg = msg)
-    #     else:
-    #         msg = 'Incorrect username / password !'
-    return render_template('login.html', msg = msg)
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        db.execute('SELECT * FROM account WHERE username = % s AND password = % s', (username, password, ))
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return redirect('/home1')
 
 # Add data route
 @app.route("/add", methods=['GET', 'POST'])
@@ -132,10 +119,8 @@ def add_company():
            status =company_status,
            location = company_location
         )
-        # c = conn.cursor()
-        # c.execute('''insert into company("project","status","location") values ("?", "?",  "?")''')
-        # conn.commit()
-        # c.close()
+       
+        db.execute('''insert into companys("project","status","location") values ("?", "?",  "?")''')
         app.app_context().push()
         db.session.add(add_detail)
         db.session.commit()
